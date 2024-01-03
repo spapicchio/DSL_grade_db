04e767fb07ba79b45d1d5aaf994fea66c73d9b08
Simone Papicchio <56116742+spapicchio@users.noreply.github.com>
2024-01-03 12:33:08 +0100

    feat add MongoDb database for DSL student ID
    * feat add MongoDatabaseIdDatabase
        * feat add_student_id() adds only if it does not exist
        * feat remove_student_id()
        * feat update_student_id() substitute student_id but not the ObjectId
        * !feat get_db_id_from() get the ObjectId associated with the studentId. This will be the connections in the db
        * feat close() closes and save the changes
    * remove DSLDatabaseIdDatabase
    * update test for dsl student id
    * add requirements.txt for pip installation

--- dsl_grade_db/dsl_student_id_database.py
+++ dsl_grade_db/dsl_student_id_database.py
@@ -1,70 +1,31 @@
-import json
-import os
+from bson import ObjectId
+from pymongo import MongoClient
 
 
-class DSLDatabaseIdDatabase:
-    """
-       DSLDatabaseIdDatabase - A class for managing a simple student ID database with a counter.
-
-       Attributes:
-           db_path (str): The path to the directory where the database file is stored.
-           db_file_path (str): The full path to the JSON database file.
-           db_json (dict): The dictionary representation of the database. The structure is as follows:
-               {
-                   "counter": 0,
-                   "student_id_1": 1,
-                   "student_id_2": 2,
-                   ...
-               }
-
-       Methods:
-           __init__(db_id_path): Initializes the DSLDatabaseIdDatabase object.
-           add_student_id(student_id: str): Adds a new student ID to the database.
-           update_student_id(student_id: str, new_student_id: str): Updates an existing student ID in the database.
-           remove_student_id(student_id: str): Removes a student ID from the database.
-           get_student_id(student_id: str) -> int: Retrieves the counter value for a given student ID.
-           _init_db() -> dict: Initializes the database by loading existing data or creating a new one.
-           _save_db(): Saves the current state of the database to the JSON file.
-
-       Example Usage:
-           # Create a new instance of DSLDatabaseIdDatabase
-           db = DSLDatabaseIdDatabase("/path/to/database")
-
-           # Add a new student ID
-           db.add_student_id("john_doe")
-
-           # Update a student ID
-           db.update_student_id("john_doe", "jane_doe")
-
-           # Remove a student ID
-           db.remove_student_id("jane_doe")
-
-           # Get the counter value for a student ID
-           counter_value = db.get_student_id("john_doe")
-
-       Note:
-           - The database file is a JSON file named 'student_id_db.json' located in the specified directory.
-           - The database structure includes a counter for tracking the number of student IDs.
-           - The database is automatically initialized when an instance of DSLDatabaseIdDatabase is created.
-           - Changes to the database are persisted to the file using the _save_db method.
-       """
-
-    def __init__(self, db_id_path):
-        self.db_path = db_id_path
-        self.db_file_path = os.path.join(self.db_path, 'student_id_db.json')
-        self.db_json = self._init_db()
+class MongoDatabaseIdDatabase:
+    def __init__(self):
+        self.client = MongoClient()
+        self.db = self.client["DSL_grade_dbs"]
+        self.collection = self.db["student_id_in_db"]
 
     def add_student_id(self, student_id: str):
         """
-        Add a new student ID to the database.
+        Add a new student ID to the database only if it does not exist.
 
         Args:
             student_id (str): The student ID to be added.
         """
-        if student_id not in self.db_json:
-            self.db_json['counter'] += 1
-            # create only if it does not exist yet
-            self.db_json[student_id] = self.db_json['counter']
+        if not self.collection.find_one({"student_id": student_id}):
+            self.collection.insert_one({"student_id": student_id})
+
+    def remove_student_id(self, student_id: str):
+        """
+        Removes a student ID from the database.
+
+        Args:
+            student_id (str): The student ID to be removed.
+        """
+        self.collection.delete_one({"student_id": student_id})
 
     def update_student_id(self, student_id: str, new_student_id: str):
         """
@@ -74,22 +35,12 @@ class DSLDatabaseIdDatabase:
             student_id (str): The existing student ID to be updated.
             new_student_id (str): The new student ID to replace the existing one.
         """
-        self.db_json[new_student_id] = self.db_json[student_id]
-        del self.db_json[student_id]
+        self.collection.update_one({"student_id": student_id},
+                                   {"$set": {"student_id": new_student_id}})
 
-    def remove_student_id(self, student_id: str):
+    def get_db_id_from(self, student_id: str) -> ObjectId | None:
         """
-        Removes a student ID from the database.
-
-        Args:
-            student_id (str): The student ID to be removed.
-        """
-        del self.db_json[student_id]
-        self.db_json['counter'] -= 1
-
-    def get_student_id(self, student_id: str):
-        """
-        Retrieves the counter value for a given student ID.
+        Retrieves the ObjectID value for a given student ID.
 
         Args:
             student_id (str): The student ID for which to retrieve the counter value.
@@ -97,33 +48,9 @@ class DSLDatabaseIdDatabase:
         Returns:
             int: The counter value associated with the specified student ID.
         """
-        return self.db_json[student_id]
-
-    def _init_db(self) -> dict:
-        """
-        Initializes the database by loading existing data or creating a new one.
-
-        Returns:
-            dict: The initialized database represented as a dictionary.
-        """
-        if os.path.exists(self.db_file_path):
-            with open(self.db_file_path, 'r') as f:
-                db_dict = json.load(f)
-
-        else:
-            if not os.path.exists(self.db_path):
-                os.makedirs(self.db_path)
-            db_dict = dict()
-            db_dict['counter'] = 0
-        return db_dict
-
-    def _save_db(self):
-        """
-        Append the current state of the database to the JSON file.
-        """
-        with open(self.db_file_path, "a") as fp:
-            json.dump(self.db_json, fp)
+        document = self.collection.find_one({"student_id": student_id})
+        return document["_id"] if document is not None else None
 
     def close(self):
         """Close the database"""
-        self._save_db()
+        self.client.close()
--- test/test_dsl_student_id_database.py
+++ test/test_dsl_student_id_database.py
@@ -1,93 +1,69 @@
-import shutil
-
 import pytest
+from bson import ObjectId
 
-from dsl_grade_db.dsl_student_id_database import DSLDatabaseIdDatabase
+from dsl_grade_db.dsl_student_id_database import MongoDatabaseIdDatabase
 
 
 # Fixture to create an instance of DSLDatabaseIdDatabase for testing
 @pytest.fixture
-def test_db_instance(tmpdir):
-    # Use a temporary directory for testing
-    test_db_path = tmpdir.mkdir("test_database")
-    test_db_instance = DSLDatabaseIdDatabase(str(test_db_path))
-    return test_db_instance
+def mongo_database():
+    db = MongoDatabaseIdDatabase()
+    yield db
+    db.close()
 
 
 # Test for adding a student ID
-def test_add_student_id(test_db_instance):
-    test_db_instance.add_student_id("john_doe")
-    assert test_db_instance.db_json['counter'] == 1
-    assert test_db_instance.db_json['john_doe'] == 1
+def test_add_student_id(mongo_database):
+    student_id = "123"
+    mongo_database.add_student_id(student_id)
+    assert mongo_database.collection.find_one({"student_id": student_id}) is not None
+    # remove the student ID otherwise it will be present in the database!
+    mongo_database.remove_student_id(student_id)
 
 
-def test_add_student_id_twice(test_db_instance):
+def test_add_student_id_twice(mongo_database):
     """the counter must not increase"""
-    test_db_instance.add_student_id("john_doe")
-    test_db_instance.add_student_id("john_doe")
-    assert test_db_instance.db_json['counter'] == 1
-    assert test_db_instance.db_json['john_doe'] == 1
+    student_id = "123"
+    mongo_database.add_student_id(student_id)
+    mongo_database.add_student_id(student_id)
+    assert mongo_database.collection.find_one({"student_id": student_id}) is not None
+    assert mongo_database.collection.count_documents({"student_id": student_id}) == 1
+    # remove the student ID otherwise it will be present in the database!
+    mongo_database.remove_student_id(student_id)
 
 
-def test_add_multiple_student_id(test_db_instance):
-    """the counter must not increase"""
-    test_db_instance.add_student_id("john_doe")
-    test_db_instance.add_student_id("simone_doe")
-    assert test_db_instance.db_json['counter'] == 2
-    assert test_db_instance.db_json['john_doe'] == 1
-    assert test_db_instance.db_json['simone_doe'] == 2
+def test_remove_student_id(mongo_database):
+    student_id = "456"
+    mongo_database.add_student_id(student_id)
+    mongo_database.remove_student_id(student_id)
+    assert mongo_database.collection.find_one({"student_id": student_id}) is None
 
 
-def test_add_multiple_student_id_and_remove(test_db_instance):
-    """the counter must not increase"""
-    test_db_instance.add_student_id("john_doe")
-    test_db_instance.add_student_id("simone_doe")
-    test_db_instance.remove_student_id("john_doe")
-    assert test_db_instance.db_json['counter'] == 1
-    assert test_db_instance.db_json['simone_doe'] == 2
+def test_update_student_id(mongo_database):
+    student_id = "789"
+    new_student_id = "999"
+    mongo_database.add_student_id(student_id)
+    key_student_id = mongo_database.get_db_id_from(student_id)
+    mongo_database.update_student_id(student_id, new_student_id)
+    key_new_student_id = mongo_database.get_db_id_from(new_student_id)
+    assert mongo_database.collection.find_one({"student_id": student_id}) is None
+    assert mongo_database.collection.find_one({"student_id": new_student_id}) is not None
+    assert key_student_id == key_new_student_id  # same key in the database
+    # remove the student ID otherwise it will be present in the database!
+    mongo_database.remove_student_id(new_student_id)
 
 
-# Test for updating a student ID
-def test_update_student_id(test_db_instance):
-    test_db_instance.add_student_id("john_doe")
-    test_db_instance.update_student_id("john_doe", "jane_doe")
-    assert 'john_doe' not in test_db_instance.db_json
-    assert test_db_instance.db_json['jane_doe'] == 1
-    assert test_db_instance.db_json['counter'] == 1
+def test_get_db_id_from(mongo_database):
+    student_id = "111"
+    mongo_database.add_student_id(student_id)
+    db_id = mongo_database.get_db_id_from(student_id)
+    assert db_id is not None
+    assert isinstance(db_id, ObjectId)
+    # remove the student ID otherwise it will be present in the database!
+    mongo_database.remove_student_id(student_id)
 
 
-# Test for removing a student ID
-def test_remove_student_id(test_db_instance):
-    test_db_instance.add_student_id("john_doe")
-    test_db_instance.remove_student_id("john_doe")
-    assert 'john_doe' not in test_db_instance.db_json
-    assert test_db_instance.db_json['counter'] == 0
-
-
-# Test for getting a student ID
-def test_get_student_id(test_db_instance):
-    test_db_instance.add_student_id("john_doe")
-    counter_value = test_db_instance.get_student_id("john_doe")
-    assert counter_value == 1
-
-
-# Test for initializing the database with existing data
-def test_init_db_with_data(tmpdir):
-    test_db_path = tmpdir.mkdir("test_database")
-    db_instance = DSLDatabaseIdDatabase(test_db_path)
-
-    # Add some initial data
-    db_instance.add_student_id("john_doe")
-    db_instance.add_student_id("jane_doe")
-    db_instance.close()
-
-    # Create a new instance to test initialization
-    new_instance = DSLDatabaseIdDatabase(test_db_path)
-
-    # Check if the data is loaded correctly
-    assert new_instance.db_json['counter'] == 2
-    assert new_instance.db_json['john_doe'] == 1
-    assert new_instance.db_json['jane_doe'] == 2
-    new_instance.close()
-
-
+def test_get_db_id_from_nonexistent_student_id(mongo_database):
+    student_id = "nonexistent"
+    db_id = mongo_database.get_db_id_from(student_id)
+    assert db_id is None
