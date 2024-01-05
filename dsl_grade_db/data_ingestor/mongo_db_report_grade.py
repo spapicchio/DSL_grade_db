ebe1bdf87f3327d19b5a3832f790af6b7cf25825
Simone Papicchio <56116742+spapicchio@users.noreply.github.com>
2024-01-05 17:36:44 +0100

    feat add mongo_db_report_grade.py as data ingestor
    * feat add mongo_db_report_grade.py
        * !core idea is to use the date present in the teams to associate the team with the leaderboard and the report grade. The project id is taken from the db_id present in student
        * feat add consume_documents() add report grade only to the project with the same project ID or create new one
    * feat add test_mongo_db_report_grade.py

---
+++ dsl_grade_db/data_ingestor/mongo_db_report_grade.py
@@ -0,0 +1,53 @@
+import pandas as pd
+from pymongo import MongoClient
+
+from dsl_grade_db.mongo_db_student_grade import MongoDBStudentGrade
+
+
+class MongoDBReportGrade:
+    def __init__(self, report_collection_name="report_grade",
+                 student_collection_name="student_grade",
+                 report_csv_file_path="report_grade.csv"):
+        self.client = MongoClient()
+        self.db = self.client["DSL_grade_dbs"]
+        self.report_coll = self.db[report_collection_name]
+        self.student_coll: MongoDBStudentGrade = self.db[student_collection_name]
+        self.report_csv_file_path = report_csv_file_path
+
+    def _read_and_insert(self, df, collection):
+        """the project ID is added automatically"""
+        project_id = self.student_coll.db_id.get_project_id()
+        df['project_id'] = project_id
+        collection.insert_many(df.to_dict('records'))
+
+    def consume_documents(self):
+        self._read_and_insert(pd.read_csv(self.report_csv_file_path), self.report_coll)
+        for report in self.report_coll.find():
+            student_id = report['Matricola']
+            student = self.student_coll.get_student(student_id)
+            project_grades = self._update_project_grade(report=report,
+                                                        project_grades=student[
+                                                            'project_grades'])
+            self.student_coll.update_student_project_grade(student_id, project_grades)
+        self.report_coll.drop()
+
+    def _update_project_grade(self, report, project_grades: list[dict]):
+        project_ids = [project['project_id'] for project in project_grades]
+        if report['project_id'] in project_ids:
+            # update the project grade
+            for project in project_grades:
+                if project['project_id'] == report['project_id']:
+                    if 'report_grade' not in project:
+                        project['report_grade'] = float(report['Final score'])
+                        project['final_grade'] += float(report['Final score'])
+                        project['report_info'] = report
+                        break
+        else:
+            # initialize the project document
+            project_grades.append({
+                'project_id': str(report['project_id']),
+                'report_grade': float(report['Final score']),
+                'final_grade': float(report['Final score']),
+                'report_info': report
+            })
+        return project_grades
